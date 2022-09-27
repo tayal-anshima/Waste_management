18517634101cd4a8c447d9866f3738073d430dda
Anshima Tayal <95869249+tayal-anshima@users.noreply.github.com>
2022-09-27 20:47:00 +0530

    Update Footer.jsx

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='bg-red-200' id="Footer">
+    <div className='bg-red-200 h-fit' >
        This is footer
     </div>
   )
