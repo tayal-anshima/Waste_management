05d1f1f0bdea7eb691f26f1e66ac1fac2a587609
Anshima Tayal <95869249+tayal-anshima@users.noreply.github.com>
2022-09-28 02:26:48 +0530

    login page linked to collector and citizen

---
+++ src/pages/GarbageCollector/CollectorLogedin.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import img from '../../assets/images/home.jpg';
+import CollectorLanding from './CollectorLanding';
+export default function CitizenHome() {
+   
+   return (
+    <div className='h-screen'>
+      
+       <div className=" sm:flex bg-teal-900 h-fit">
+          <div className=" sm:w-1/2 sm:my-10 text-center sm:text-justify ">
+            <CollectorLanding/>
+          </div>
+          <div className=" sm:w-1/2 grid  justify-items-center sm:justify-items-end ">
+             <img className="h-full hidden sm:block " src={img} alt=""/>
+          </div>
+       </div>
+    </div>
+  )
+}
