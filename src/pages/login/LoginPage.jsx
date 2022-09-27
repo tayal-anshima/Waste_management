6f731c300827f5a5837dcde037a0bc1ae4a3aa7a
Anshima Tayal <95869249+tayal-anshima@users.noreply.github.com>
2022-09-27 22:20:31 +0530

    Update LoginPage.jsx

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,10 +1,18 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 
 export default function LoginPage(props) {
   const[username,setUsername]=useState("");
   const[password,setPassword]=useState("");
-  const[category,setCategory]=useState("");
+  const[category,setCategory]=useState("Citizen");
+
+  function handleChange (e) {
+    setCategory(e.target.value);
+  }
+
+  function printValue(e) {
+    console.log(e);
+  }
   const handleSubmit=async(e)=>{
     e.preventDefault();
     //code
@@ -16,20 +24,21 @@ export default function LoginPage(props) {
                 <h2 className='text-4xl font-bold text-center py-6'>Login</h2>
                 <div className='flex flex-col py-2'>
                     <label>Username</label>
-                    <input className='border p-2' type="text"  onChange={(e) => setUsername(e.target.value)}/>
+                    <input className='border p-2' type="text"  onChange={(e) => setUsername(e.target.value)}required/>
                 </div>
                 <div className='flex flex-col py-2'>
                     <label>Password</label>
-                    <input className='border p-2' type="password"  onChange={(e) => setPassword(e.target.value)}/>
+                    <input className='border p-2' type="password"  onChange={(e) => setPassword(e.target.value)} required/>
                 </div>
                 <div className='flex flex-col py-2'>
                     <label>Category</label>
-                     <select className="text-black" >
-                       <option value="Citizen"onChange={(e) => setCategory(e.target.value)} >Citizen</option>
-                       <option value="Garbage Collector"onChange={(e) => setCategory(e.target.value)} >Garbage Collector</option>
+                     <select className="text-black" required onChange={ handleChange }>
+                       <option value="Citizen" >Citizen</option>
+                       <option value="Garbage Collector" >Garbage Collector</option>
                      </select>
                 </div>
-                <Link to={`${category==="Citizen"?"/citizen":"/collector"}`}>
+                
+                <Link to={`${category==="Citizen"?"/citizens":"/collector"}`}>
                 <button className='border w-full my-5 py-2 bg-white  text-teal-900' >Login</button>
                 </Link>
                 
@@ -37,3 +46,4 @@ export default function LoginPage(props) {
     </div>
   )
 }
+
