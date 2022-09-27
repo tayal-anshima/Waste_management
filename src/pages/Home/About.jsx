93e32465c867203a2265bedfda470ab198ca3382
Anshima Tayal <95869249+tayal-anshima@users.noreply.github.com>
2022-09-27 20:46:26 +0530

    Update About.jsx

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className="bg-slate-600">
+    <div className="bg-slate-600 h-fit">
        this is About
     </div>
   )
