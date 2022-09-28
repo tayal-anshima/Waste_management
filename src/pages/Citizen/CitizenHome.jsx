737388aeee9752338d0599f9b31f2506434ec8a4
Anshima Tayal <95869249+tayal-anshima@users.noreply.github.com>
2022-09-28 13:44:09 +0530

    modified citizenLanding page

--- src/pages/Citizen/CitizenHome.jsx
+++ src/pages/Citizen/CitizenHome.jsx
@@ -1,20 +1,24 @@
 import React,{useState} from 'react'
-import img from '../../assets/images/home.jpg';
+import vdo from '../../assets/Video/ComponentsVdo.mp4';
 import SignUp from './SignUp';
 import CitizenLanding from './CitizenLanding';
+
 export default function CitizenHome() {
    const[signed,setSigned]=useState(false);
    
   return (
-    <div className='h-screen'>
-       <div className=" sm:flex bg-teal-900 h-fit">
-          <div className=" sm:w-1/2 sm:my-10 text-center sm:text-justify ">
-            {signed?(<CitizenLanding/>):(<SignUp signed={setSigned}/>)};
-          </div>
-          <div className=" sm:w-1/2 grid  justify-items-center sm:justify-items-end ">
-             <img className="h-full hidden sm:block " src={img} alt=""/>
-          </div>
+   <div>
+   <div className=" sm:flex h-fit">
+       <div className="bg-teal-900 sm:w-1/2 text-center sm:text-justify p-10">
+         {signed?(<CitizenLanding/>):(<SignUp signed={setSigned}/>)};
+       </div>
+       <div className="bg-teal-900 sm:w-1/2 grid justify-items-center sm:justify-items-end ... ">
+          <video autoPlay loop muted className='mt-12 sm:mt-20 h-4/6 rounded-xl sm:rounded-l-full ...'>
+             <source src={vdo} type="video/mp4"></source>
+         </video>
        </div>
     </div>
+ </div>
   )
 }
+
